74eb775417cb4522d90af1b48c2a542fb30e6e29
syamvelaga <syamvelaga64@gmail.com>
2023-09-01 08:48:45 +0000

    Inprogress...

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,65 @@
 // Write your code here
+
+import {formatDistanceToNow} from 'date-fns'
+
+import './index.css'
+
+function CommentItem(props) {
+  const {each, isLiked} = props
+  // console.log(each)
+  const {name, comment} = each
+
+  const commentCreatedTime = formatDistanceToNow(new Date())
+
+  console.log(formatDistanceToNow(new Date()))
+
+  const firstLetter = name[0].toUpperCase()
+
+  function HandelLikebutton() {
+    console.log(!isLiked)
+  }
+
+  return (
+    <li>
+      <div>
+        <div className="name-comment-container">
+          <p className="round">{firstLetter}</p>
+          <div className="name-container">
+            <h1 className="name">
+              {name} <span className="formated">{commentCreatedTime}</span>
+            </h1>
+
+            <p className="comment">{comment}</p>
+          </div>
+        </div>
+        <div className="like-delete-container">
+          <div className="like-container">
+            <button
+              onClick={HandelLikebutton()}
+              className="delete"
+              type="button"
+            >
+              <img
+                alt="like"
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
+              />
+            </button>
+
+            <p className="like-name">Like</p>
+          </div>
+          <div>
+            <button className="delete" type="button">
+              <img
+                alt="delete"
+                src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </li>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -12,8 +12,92 @@
 
 import {Component} from 'react'
 
+import {v4 as uuidv4} from 'uuid'
+
+import CommentItem from '../CommentItem'
+
+import './index.css'
+
 export default class Comments extends Component {
+  state = {name: '', comment: '', isLiked: false, commentList: []}
+
+  handelName = e => {
+    const yourname = e.target.value
+
+    this.setState({name: yourname})
+  }
+
+  handelComment = e => {
+    const yourcomment = e.target.value
+
+    this.setState({comment: yourcomment})
+  }
+
+  handleSubmitEvent = e => {
+    e.preventDefault()
+
+    const {name, comment, commentList} = this.state
+
+    const newCommentObj = {id: uuidv4(), name, comment}
+
+    this.setState({
+      commentList: [...commentList, newCommentObj],
+      name: '',
+      comment: '',
+    })
+  }
+
   render() {
-    return <div>Hi</div>
+    const {name, comment, commentList, isLiked} = this.state
+    // console.log(name, comment)
+    // console.log(commentList)
+
+    return (
+      <div className="main-container">
+        <div className="comments-create-container">
+          <form onSubmit={this.handleSubmitEvent}>
+            <h1>Comments</h1>
+            <br />
+            <p>Say Something about 4.0 Technologies</p>
+            <input
+              value={name}
+              onChange={this.handelName}
+              type="text"
+              placeholder="Your Name"
+            />
+            <br />
+            <br />
+            <textarea
+              value={comment}
+              onChange={this.handelComment}
+              rows="7"
+              cols="25"
+              type="text"
+              placeholder="Your Comment"
+            />
+            <br />
+            <br />
+            <button type="submit">Add Comment</button>
+          </form>
+          <div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              alt="comments"
+            />
+          </div>
+        </div>
+        <br />
+        <hr />
+        <div className="count-comments">
+          <p className="count">2</p>
+          <p>Comments</p>
+        </div>
+        <ul className="comment-list-container">
+          {commentList.map(each => (
+            <CommentItem isLiked={isLiked} key={each.id} each={each} />
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
