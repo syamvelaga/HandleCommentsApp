9391eef543b1f9e99c9a5dbb9ee73655d641001c
syamvelaga <syamvelaga64@gmail.com>
2023-09-01 04:04:25 +0000

    Strting...

---
+++ src/components/Comments/index.js
@@ -0,0 +1,19 @@
+// const initialContainerBackgroundClassNames = [
+//   'amber',
+//   'blue',
+//   'orange',
+//   'emerald',
+//   'teal',
+//   'red',
+//   'light-blue',
+// ]
+
+// Write your code here
+
+import {Component} from 'react'
+
+export default class Comments extends Component {
+  render() {
+    return <div>Hi</div>
+  }
+}
